880e78e909d36133bc9f1211e083dfd566a39338
Giovanni Dias Silva E Costa <giovanni.costa@sidia.com>
2022-12-13 14:52:25 -0400

    Prisma Schema Updated
    Signed-off-by: Giovanni Dias Silva E Costa <giovanni.costa@sidia.com>

--- src/createSubst.dto.ts
+++ src/createSubst.dto.ts
@@ -1,5 +1,25 @@
 export type createSubstDto = {
     id?: number
+    chericId: number
     name: string
     formula: string
+    imageURL: string
+    molWeight? : string
+    normalBoilingT?: string
+    normalFreezingT?: string
+    triplePointT?: string
+    triplePointP?: string
+    criticalT?: string
+    criticalP?: string
+    criticalV?: string
+    critialComp?: string
+    accFactor?: string
+    vaporPressure?: string
+    heatCapGas?: string
+    heatCapLiq?: string
+    viscosityGas?: string
+    viscosityLiq?: string
+    thermalCondGas?: string
+    thermalConsLiq?: string
+    surfaceTension?: string
 }
